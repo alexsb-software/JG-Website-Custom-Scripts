05a1f437b6f294b3bf240b05f1958ad7cddbed41
Amr Fathy <amruafathy@gmail.com>
2017-06-26 11:54:16 +0200

    Make tabs height dynamic when embedded

---
+++ custom.js
@@ -0,0 +1,29 @@
+/**
+ * This script should be in the parent page, in which
+ * `tabs.html` is included.
+ */
+
+// JG17 webpage iframe height modification script
+window.onload = function () {
+    adjustHeight();
+
+    var object = x.contentDocument.getElementById("tab1");
+    object.addEventListener("click", adjustHeight);
+
+    var object = x.contentDocument.getElementById("tab2");
+    object.addEventListener("click", adjustHeight);
+
+    var object = x.contentDocument.getElementById("tab3");
+    object.addEventListener("click", adjustHeight);
+
+    var object = x.contentDocument.getElementById("tab4");
+    object.addEventListener("click", adjustHeight);
+
+    var object = x.contentDocument.getElementById("tab5");
+    object.addEventListener("click", adjustHeight);
+}
+  
+function adjustHeight() {
+    var x = document.getElementById("jg17-webpage-frame");
+    x.height = x.contentDocument.body.clientHeight;
+}
